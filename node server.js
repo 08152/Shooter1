56db380a68cbbd84056abf75e9fbc0bac1659e1e
08152 <RiskyLive@proton.me>
2026-07-03 13:21:40 +0200

    Update node server.js

--- node server.js
+++ node server.js
@@ -2,17 +2,16 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const PORT = 3000;
 
-// static files (HTML, JS, CSS)
-app.use(express.static(path.join(__dirname)));
+// static files
+app.use(express.static(__dirname));
 
-// start page
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-// start server
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-  console.log("Server läuft auf http://localhost:" + PORT);
+  console.log("Server läuft auf Port " + PORT);
 });
